5ec44f8a095cdf46a7fc26ea97fc33d29bed9968
yuanzihan <yuanzihan@longfor.com>
2021-12-17 11:06:27 +0800

    init

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from senta import Senta
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+my_senta = Senta()
+
+# 获取目前支持的情感预训练模型, 我们开放了以ERNIE 1.0 large(中文)、ERNIE 2.0 large(英文)和RoBERTa large(英文)作为初始化的SKEP模型
+print(my_senta.get_support_model()) # ["ernie_1.0_skep_large_ch", "ernie_2.0_skep_large_en", "roberta_skep_large_en"]
+
+# 获取目前支持的预测任务
+print(my_senta.get_support_task()) # ["sentiment_classify", "aspect_sentiment_classify", "extraction"]
+
+# 选择是否使用gpu
+use_cuda = False # 设置True or False
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 预测中文评价对象级的情感分类任务
+my_senta.init_model(model_class="ernie_1.0_skep_large_ch", task="aspect_sentiment_classify", use_cuda=use_cuda)
+texts = ["百度是一家高科技公司"]
+aspects = ["百度"]
+result = my_senta.predict(texts, aspects)
+print(result)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 预测中文观点抽取任务
+my_senta.init_model(model_class="ernie_1.0_skep_large_ch", task="extraction", use_cuda=use_cuda)
+texts = ["唐 家 三 少 ， 本 名 张 威 。"]
+result = my_senta.predict(texts, aspects)
+print(result)
